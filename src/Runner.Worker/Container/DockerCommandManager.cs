741deafe5e19e5b8c58628934ebd6dfb771390ca
retzero <ticce@naver.com>
2023-09-04 21:31:37 +0900

    More debug log

--- src/Runner.Worker/Container/DockerCommandManager.cs
+++ src/Runner.Worker/Container/DockerCommandManager.cs
@@ -387,16 +387,23 @@ namespace GitHub.Runner.Worker.Container
 
         private async Task<int> ExecuteDockerCommandAsync(IExecutionContext context, string command, string options, IDictionary<string, string> environment, EventHandler<ProcessDataReceivedEventArgs> stdoutDataReceived, EventHandler<ProcessDataReceivedEventArgs> stderrDataReceived, CancellationToken cancellationToken = default(CancellationToken))
         {
+            context.Output($"/CODE/ Macnine Name: [{Environment.MachineName}]");
+            context.Output($"/CODE/ command: [{command}]");
+            context.Output($"/CODE/ options: [{options}]");
             var originalImageName = "";
             if (Environment.MachineName.StartsWith("code-runner-greenback-") || Environment.MachineName.StartsWith("code-runner-metronome-")) {
+                context.Output($"/CODE/ + Machine Name");
                 if (command.Equals("pull") || command.EndsWith(" pull")) {
+                    context.Output($"/CODE/ + Docker Pull");
                     if (!options.Contains(" ") && options.StartsWith("ghcr.io/")) {
+                        context.Output($"/CODE/ + Docker Image");
                         originalImageName = options;
                         options = options.Replace("ghcr.io", "ghcr-docker-remote.bart.sec.samsung.net");
                         context.Output($"/CODE/ Switch to BART. {originalImageName} {options}");
                     }
                 }
             }
+            context.Output($"/CODE/ Final options: [{options}]");
             string arg = $"{command} {options}".Trim();
             context.Command($"{DockerPath} {arg}");
 
